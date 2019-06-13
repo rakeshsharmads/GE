7af09c02f287fda7489bb24b62c61d46de428336
rakeshsharmads <51693942+rakeshsharmads@users.noreply.github.com>
2019-06-13 04:43:23 +0000

    Update Backend_ChartsData.cpp

--- Backend_ChartsData.cpp
+++ Backend_ChartsData.cpp
@@ -9,7 +9,6 @@ QVector<double> BackendChartsData::getChartData()
 {
     QVector<double> yValue;
     yValue.clear();
-
    for (int i = 0; i <= 50; i++) 
     {
         yValue.push_back(sin(30*i)/*random()*/);
